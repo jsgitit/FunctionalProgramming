a4f30de74c9a5d7f6d09440e97ded663b6ba6559
Jon Smart <jsmartware@gmail.com>
2020-07-30 10:18:10 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FunctionalProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    
+    
+}
