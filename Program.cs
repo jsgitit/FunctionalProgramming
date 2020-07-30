c3d63552431dca2247cd98e06e6e9fe8f9f2428b
Jon Smart <jsmartware@gmail.com>
2020-07-30 10:38:29 -0700

    Created higher order function Find(), using lazy evaluation

--- Program.cs
+++ Program.cs
@@ -1,15 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FunctionalProgramming
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = new[] { 3, 5, 7, 9, 11, 13 };
+            foreach (var prime in numbers.Find(IsPrime).Take(2))
+            {
+                Console.WriteLine("Prime number found: ", prime);
+            }
+        }
+
+        private static IEnumerable<int> GetLazyRandomNumber(int max)
+        {
+            var rand = new Random();
+            while (true)
+            {
+                yield return rand.Next(max);
+            }
+
+        }
+
+        // Find() is a lazy, higher order function, yield returning one result at a time, 
+        // saving processing time.
+        private static IEnumerable<int> Find(this IEnumerable<int> values, Func<int, bool> test)
+        {
+            foreach (var number in values)
+            {
+                Console.WriteLine("Testing value {0}", number);
+                if (test(number))
+                {
+                    yield return number;
+                }
+            }
+        }
+
+        private static bool IsPrime(int number)
+        {
+            bool result = true;
+            for (long i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    result = false;
+                    break;
+
+                }
+            }
+            return result;
         }
     }
-    
-    
 }
